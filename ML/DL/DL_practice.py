fe7b698ed9929a77d8d4fbda9a2ae90d7fae13ec
jimin <jiminseo0307@gmail.com>
2021-06-03 02:03:58 +0900

    LSM

--- ML/DL/DL_practice.py
+++ ML/DL/DL_practice.py
@@ -16,6 +16,56 @@
 
 import numpy as np
 
+# # 최소 제곱법 Least Squares Method
+# > 단순선형회귀모형 : 모형 내 설명변수(x)가 1개만 있는 모형<br>
+# > 1개의 설명변수만으로 반응변수 Y에 대한 영향을 파악하기 위해 사용
+
+# +
+# y = a*x + b
+# parameter : a, b
+# -
+
+x = [2, 4, 6, 8]
+y = [81, 93, 91, 97]
+
+# mean
+mx = np.mean(x)
+my = np.mean(y)
+
+# LSM (a)분모 : sum of (x - mean(x)) ** 2
+divisor = sum([(i - mx)**2 for i in x])
+
+
+# LSM (a)분자 : sum of (x[i] - mx) * (y[i] - my)
+def top(x, mx, y, my):
+    d = 0
+    for i in range(len(x)):
+        d += (x[i] - mx) * (y[i] - my)
+    return d
+
+
+dividend = top(x, mx, y, my)
+
+# +
+# 기울기 a
+a = dividend / divisor
+
+# y절편
+b = my - (mx*a)
+# -
+
+print('x 평균', mx)
+print('y 평균', my)
+print('분모', divisor)
+print('분자', dividend)
+print('\n')
+print('기울기(a)', a)
+print('y 절편', b)
+
+# # 평균 제곱 오차 Mean Suqare Error, MSE
+# > 여러 개의 입력값을 계산할 때<br>
+# > 임의의 선을 긋고, 평가 & 수정 (오차 평가 알고리즘)
+
 # +
 # 기울기 a, y 절편
 fake_a_b = [3, 76]
